860ecbf4ad4d9fc8f53a9c1dbf2d45282c7735f6
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-09-01 13:38:52 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -9,10 +9,8 @@ admins = {}
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 STRING_SESSION = getenv("STRING_SESSION")
-BOT_USERNAME = getenv("BOT_USERNAME")
 
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
